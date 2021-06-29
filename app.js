0132289f3a754579567373fc6d9ffdaa8a8b55e6
DEEPJYOTSINGHKAPOOR <kapoordeepjyotsingh29@gmail.com>
2021-06-29 20:00:11 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -199,8 +199,14 @@ app.post("/blog_compose", function (req, res) {
 
 
 
+let port = process.env.PORT ;
+
+if(port == null || port == ""){
+    port = 3000 ;
+}
 
 
-app.listen(3000, function () {
+app.listen(port, function () {
+    console.log("server has started successfully.");
     return;
 });
